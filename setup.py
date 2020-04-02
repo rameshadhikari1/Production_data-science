063236feca530efd9c49ae1ad96ed8f2e5ecd864
Ramesh Adhikari <radhikari@utexas.edu>
2020-04-01 21:40:27 -0500

    data_pipeline

--- setup.py
+++ setup.py
@@ -20,16 +20,25 @@ setup(
         'Programming Language :: Python :: 3',
     ],
     # Substitute <github_account> with the name of your GitHub account
-    url='https://github.com/<github_account>/titanic_datascience',
-    author='Filippo Bovo',  # Substitute your name
-    author_email='filippo@satalia.com',  # Substitute your email
+    url='https://github.com/rameshadhikari1/titanic_data-science',
+    author='Ramesh Adhikari',  # Substitute your name
+    author_email='radhikari@utexas.edu',  # Substitute your email
     license='MIT',
     packages=['titanic'],
     install_requires=[
 	'pypandoc>=1.4', 
+	'watermark>=1.8.1',
+	'pandas>=0.24.2',
+	'scikit-learn>=0.20.3',
+	'scipy>=1.2.1',
+	'matplotlib>=3.0.3',
 	'pytest>=4.3.1', 
 	'pytest-runner>=4.4',
+	'click>=7.0',
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
+    entry_points='''
+        [console_scripts]
+	titanic_analysis=titanic.command_line:titanic_analysis'''
 )
